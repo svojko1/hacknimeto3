67b30e2a2bb7cd01d5cecbd5a34ef46283f806b4
Stanislav Vojtko <stano9696@gmail.com>
2025-05-24 12:59:34 +0200

    3d

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-type Theme = "dark" | "light" | "system";
+type Theme = "light";
 
 type ThemeProviderProps = {
   children: React.ReactNode;
